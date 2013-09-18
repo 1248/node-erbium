7b8251292b4083a2e96657a5cde643a4c9dd5f19
Toby Jaffey <toby@1248.io>
2013-09-18 13:56:06 +0100

    Gets copied to a fixed length buffer in erbium anyway

--- src/erbium.cc
+++ src/erbium.cc
@@ -480,14 +480,7 @@ Handle<Value> Erbium::SetHeaderToken(const Arguments& args) {
     Local<Object> bufferObj    = args[0]->ToObject();
     char*         bufferData   = Buffer::Data(bufferObj);
     size_t        bufferLength = Buffer::Length(bufferObj);
-#if 0
     coap_set_header_token(&obj->pkt_, (uint8_t *)bufferData, bufferLength);
-#else
-    char *d = (char *)malloc(bufferLength);
-    memcpy(d, bufferData, bufferLength);
-printf("token_len=%d\n", bufferLength);
-    coap_set_header_token(&obj->pkt_, (uint8_t *)d, bufferLength);
-#endif
     return scope.Close(Undefined());
 }
 
